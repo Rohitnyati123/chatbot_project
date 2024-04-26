fe4d6fe733e72980952abaf1ea988365064d43be
Rohitnyati1 <rohit.nyati11@gmail.com>
2024-04-26 10:59:20 +0530

    setup.py and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from typing import List
+
+PROJECT_NAME = "Chatbot project"
+VERSION = "0.0.1"
+DESCRIPTION = "Creating a chatbot using deep learning"
+AUTHOR_NAME = "Rohit Nyati"
+AUTHOR_EMIL = "rohitnyati5@gmail.com"
+
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+# Requriments.txt file open
+# read
+# \n ""
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as requriment_file:
+        requriment_list = requriment_file.readlines()
+        requriment_list = [requriment_name.replace("\n", "") for requriment_name in requriment_list]
+
+        if HYPHEN_E_DOT in requriment_list:
+            requriment_list.remove(HYPHEN_E_DOT)
+
+        return requriment_list
+
+setup(name=PROJECT_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR_NAME,
+      author_email=AUTHOR_EMIL,
+      packages=find_packages(),
+      install_requries = get_requirements_list()
+     )
